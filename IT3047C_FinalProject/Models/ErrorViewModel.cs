1e76a42a88affeb7790464b0d9500e89decffe0b
Sage Bushstone <sage.bushstone@gmail.com>
2025-10-20 08:47:42 -0400

    Add project files.

---
+++ IT3047C_FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT3047C_FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
