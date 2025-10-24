400e4eb255990a4c7434164c8083b8fe4d0d9556
Sage Bushstone <sage.bushstone@gmail.com>
2025-10-24 08:10:25 -0400

    Created Trail and Gear entities

---
+++ IT3047C_FinalProject/Models/Gear.cs
@@ -0,0 +1,11 @@
+﻿namespace IT3047C_FinalProject.Models
+{
+    public class Gear
+    {
+        public int GearId { get; set; }
+        public string GearName { get; set; }
+        public double Weight { get; set; }
+        public double Cost { get; set; }
+        public int RecommendedQuantity { get; set; }
+    }
+}
---
+++ IT3047C_FinalProject/Models/Trail.cs
@@ -0,0 +1,14 @@
+﻿namespace IT3047C_FinalProject.Models
+{
+    public class Trail
+    {
+        public int TrailId { get; set; }
+        public string TrailName { get; set; }
+
+        public string State { get; set; }
+
+        public string Park {  get; set; }
+        public int DifficultyLevel { get; set; }
+        public string Description { get; set; }
+    }
+}
