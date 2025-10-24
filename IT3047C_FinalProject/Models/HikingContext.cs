7d404f5f4cc2f102436d5a08d64ae014967d1c86
Sage Bushstone <sage.bushstone@gmail.com>
2025-10-24 08:16:38 -0400

    Made some changes to HikingContext

--- IT3047C_FinalProject/Models/HikingContext.cs
+++ IT3047C_FinalProject/Models/HikingContext.cs
@@ -4,5 +4,9 @@ namespace IT3047C_FinalProject.Models
 {
     public class HikingContext : DbContext
     {
+        public HikingContext(DbContextOptions<HikingContext> options) : base(options) { }
+
+        public DbSet<Trail> Trails { get; set; }
+        public DbSet<Gear> Gears { get; set; }
     }
 }
