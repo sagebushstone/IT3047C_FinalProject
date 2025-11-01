7aadfad4691db5c01ba4160fd249c6ccdd399df0
ZBetsch <betschzt@mail.uc.edu>
2025-10-31 22:35:04 -0400

    Added Trail data to the HikingContext.cs file Created TrailInitialItems migration

--- IT3047C_FinalProject/Models/HikingContext.cs
+++ IT3047C_FinalProject/Models/HikingContext.cs
@@ -54,6 +54,45 @@ namespace IT3047C_FinalProject.Models
                     RecommendedQuantity = 1
                 }
             );
+
+            modelBuilder.Entity<Trail>().HasData(
+                new Trail
+                {
+                    TrailId = 1,
+                    TrailName = "Colerain Trail",
+                    State = "Ohio",
+                    Park = "Mt. Airy Forest",
+                    DifficultyLevel = 1,
+                    Description = "The Colerain trail is an excellent beginner level trail in the beautiful Mt. Airy Forest, located on the west side of Cincinnati. As the trail is under a mile, expect an easy forest walk that can be completed in under an hour."
+                }, 
+                new Trail
+                {
+                    TrailId = 2,
+                    TrailName = "Ponderosa Trail",
+                    State = "Ohio",
+                    Park = "Mt. Airy Forest",
+                    DifficultyLevel = 3,
+                    Description = "The Ponderosa trail is an intermediate level 4 mile hiking trail in the scenic Mt. Airy Forest. This trail will take you on a meandering path through a majority of the eastern half of the forest, and runs in tandem with multiple other trails located within the forest. If you are looking for a solid challenge, this may be the trail for you."
+                },
+                new Trail
+                {
+                    TrailId = 3,
+                    TrailName = "B Loop Trail",
+                    State = "Ohio",
+                    Park = "Buttercup Valley and Parker Woods",
+                    DifficultyLevel = 2,
+                    Description = "Buttercup Valley and Parker Woods are a smaller nature reserve located on the west side of Cincinnati. The 1.4 mile long B Loop Trail takes you through both of the reserves, and acts as a more casual nature trail. Even so, there are still plenty of scenic twists, turns, bridges, and hills to keep the trail interesting even for more experienced hikers."
+                },
+                new Trail
+                {
+                    TrailId = 4,
+                    TrailName = "Shaker Trace Outer Loop",
+                    State = "Ohio",
+                    Park = "Miami Whitewater Forest",
+                    DifficultyLevel = 4,
+                    Description = "In the gorgeous Miami Whitewater Forest located in Harrison OH, hikers are treated to multiple options for trails. The main trail is the Shaker Trace Loop, which gives hikers an option between the easier 1.4 mile inner loop, or the more challenging 7.8 mile outer loop. With either choice, you will be treated to the beautiful forest, challenging inclines, refreshing waterways all on a paved pathway. If you are looking for an excellent entry level intermediate level course that’s still easy on the knees, This trail may be for you."
+                }
+            );
         }
     }
 }
