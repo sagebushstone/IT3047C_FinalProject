705401cb9c6b05d97fdbfb0fae9f60bf7622a321
ZBetsch <betschzt@mail.uc.edu>
2025-10-31 21:44:15 -0400

    Created Initial Migration Created Gear data in HikingContext.cs Created GearInitialItems db migration

--- IT3047C_FinalProject/Models/HikingContext.cs
+++ IT3047C_FinalProject/Models/HikingContext.cs
@@ -8,5 +8,52 @@ namespace IT3047C_FinalProject.Models
 
         public DbSet<Trail> Trails { get; set; }
         public DbSet<Gear> Gears { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Gear>().HasData(
+                new Gear
+                {
+                    GearId = 1,
+                    GearName = "Backpack",
+                    Weight = 0.4,
+                    Cost = 100,
+                    RecommendedQuantity = 1
+                },
+                new Gear
+                {
+                    GearId = 2,
+                    GearName = "Hiking Shoes",
+                    Weight = 0.5,
+                    Cost = 150,
+                    RecommendedQuantity = 1
+                },
+                new Gear
+                {
+                    GearId = 3,
+                    GearName = "Trekking Poles",
+                    Weight = 0.5,
+                    Cost = 100,
+                    RecommendedQuantity = 2
+                },
+                new Gear
+                {
+                    GearId = 4,
+                    GearName = "2l Water Bottle",
+                    Weight = 0.3,
+                    Cost = 50,
+                    RecommendedQuantity = 1
+                },
+                new Gear
+                {
+                    GearId = 5,
+                    GearName = "Hat",
+                    Weight = 0.1,
+                    Cost = 30,
+                    RecommendedQuantity = 1
+                }
+            );
+        }
     }
 }
